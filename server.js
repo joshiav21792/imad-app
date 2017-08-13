cf2b2f43cc1d19e1347bdecb7e25daf069905986
joshiav21792 <30953306+joshiav21792@users.noreply.github.com>
2017-08-13 15:48:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', '/article-one.html'));
+    res.sendFile(path.join(__dirname,'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', '/article-two.html'));
+    res.sendFile(path.join(__dirname,'article-two.html'));
 });
 app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', '/article-three.html'));
+    res.sendFile(path.join(__dirname,'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
